60937b233b330c4ba4432c0786b64ef0b57f9f5a
SemTenz <sem.jaeger@ziggo.nl>
2023-06-13 12:56:59 +0200

    2de

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvc2209aP4toets');
+define('DB_NAME', 'mvc2209aHERKP4toets');
 define('URLROOT', 'http://mvc-2209a-p4-toets.org/');
